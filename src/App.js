e4cd30cbc154bfd8f5fb0e588a8fd22e3b6dc2e8
Vikram Bageja <vikram.bageja@adludio.com>
2020-12-31 16:56:57 +0000

    Change name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Vikrams app
+          Vikrams app 2 deploy
         </p>
         <a
           className="App-link"
